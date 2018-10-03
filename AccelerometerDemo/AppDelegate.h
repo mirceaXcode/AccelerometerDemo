e0b5e2492803b55a39df2d99be6c7f0fbce5e512
Mircea Popescu <mirceap@gmail.com>
2018-10-03 15:21:21 -0500

    Initial Commit

---
+++ AccelerometerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccelerometerDemo
+//
+//  Created by Mircea Popescu on 10/3/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
